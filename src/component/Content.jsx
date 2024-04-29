b367724857ba6672bed6c3a3ca9a1d93ba3ebf9c
S1rCoDy <148563516+S1rCoDy@users.noreply.github.com>
2024-04-29 16:27:27 +0500

    Add files via upload

---
+++ src/component/Content.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './CSS/Conteent.css'
+
+const Content = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className='content-page'>
+            <h1>gfsajdfpoiasjdiojsaiopd</h1>
+        </div>
+      );
+
+};
+export default Content;
